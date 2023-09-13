1c3d72e46bcdb20309a56e48ed3955864fd7ec99
CalinPavel <calinpav21@gmail.com>
2023-09-13 10:51:45 +0300

    Trades lazy

--- src/App.js
+++ src/App.js
@@ -1,28 +1,28 @@
 import './App.css';
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import { Dashboard } from './components/Dashboard';
+import Login from './components/Login';
+import Register from './components/Register';
+
 import {Loading} from './components/Loading'
 import React, { Suspense } from 'react';
 
 function App() {
 
-  const Login = React.lazy(() => import("./components/Login"));
-  const Register = React.lazy(() => import("./components/Register"));
   const Trades = React.lazy(() => import("./components/RatesView"));
 
 
   return (
       <>
-        {/* <Login></Login> */}
         <Suspense fallback={<Loading/>}>
-        <BrowserRouter>
-          <Dashboard/>
-          <Routes>
-            <Route path="login" element={<Login/>}></Route>
-            <Route path="register" element={<Register/>}></Route>
-            <Route path="fxTrades" element={<Trades/>}></Route>
-          </Routes>
-        </BrowserRouter>
+          <BrowserRouter>
+            <Dashboard/>
+            <Routes>
+              <Route path="login" element={<Login/>}></Route>
+              <Route path="register" element={<Register/>}></Route>
+              <Route path="fxTrades" element={<Trades/>}></Route>
+            </Routes>
+          </BrowserRouter>
         </Suspense>
       </>
   );
